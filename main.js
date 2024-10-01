356eae3b7ee907648eb1ca067f6a1247954c716b
hyemimi <hyemi7375@gmail.com>
2024-10-01 23:54:59 +0900

    ui

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 import './style.scss'
 
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <p>프로젝트 셋팅</p>
-  </div>
-`
+const upload = document.getElementById("upload");
+const canvas = document.getElementById("imageCanvas");
+const ctx = canvas.getContext('2d');
+
+upload.addEventListener('change', (e) => {
+  const file = e.target.files[0];
+  const img = new Image();
+  img.src = URL.createObjectURL(file);
+  img.onload = () => {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+  };
+})
+
+
+
+
 
 
